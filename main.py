83acfeff8a48384346a6f4662d6596c0645de06c
mukhtar_syarief <mukhtar.syariefudin@gmail.com>
2021-12-30 21:23:29 +0700

    Introduction to Data Science with Python

---
+++ main.py
@@ -0,0 +1,37 @@
+#Python “Hello World” & Statement
+print("Helo World!")
+print("Saya Aksara,baru belajar Python.")
+
+#Variables di Python
+bil1 = 10
+print(bil1)
+Bil_2 = 20
+print(Bil_2)
+Frasa = "Halo Dunia"
+print(Frasa)
+bil1, Bil_2 = 10, 20
+print(bil1,Bil_2)
+salam = "Selamat pagi"; Penutup = "Salam Sejahtera"
+print(salam,Penutup)
+
+#Comments di Python
+#perintah pada baris ini tidak mempengaruhi program
+'''
+perintah ini tidak akan dieksekusi oleh Python 
+dan perintah ini juga tidak akan dieksekusi
+perintah ini juga tidak akan dieksekusi
+'''
+print("jadi # digunakan untuk membuat comment pada Python")
+
+#Tugas Praktek
+text = "Belajar Python di DQLab."
+print(list(text))
+print(tuple(text))
+print(set(text))
+
+#Menggunakan Library di Python
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
+ 
